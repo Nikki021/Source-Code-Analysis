03ae4f7465d3f2df779f67354f6801ed3755a58f
Nikhil Matcha <nikhil.matcha021@gmail.com>
2025-04-16 15:52:39 -0500

    version-1

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Nikhil Matcha',
+    author_email= 'nikhil.matcha021@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
